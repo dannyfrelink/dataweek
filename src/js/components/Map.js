78521cfd29bebac9cc6c5b5fbfb3d885b0a90f37
dannyfrelink <danny-frelink@hotmail.com>
2021-12-07 11:46:16 +0100

    begin aanmaak onClick functie

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -1,4 +1,8 @@
 function Map() {
+    function onClick(d) {
+        const transportType = d.currentTarget.dataset.transport;
+    }
+
     // SVG of Mickey's route
     return (
         <p>test</p>
